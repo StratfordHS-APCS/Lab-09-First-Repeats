e19e4479d7b1ec1ad402bbb0f8557312f842b8dd
Dave Avis <charles.avis@springbranchisd.com>
2020-10-20 13:29:47 +0000

    Updated tests

--- tests/TestMain.java
+++ tests/TestMain.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class TestMain {
 
   @Test
-  public void testName() {
+  public void testArrays() {
     int[][] inputs = new int[][]{
       {-99,1,2,3,4,5,6,7,8,9,10,12345},
       {10,9,8,7,6,5,4,3,2,1,-99},
